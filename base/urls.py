bc1966aff1102a213dfa23fc875e7a43b92fee32
himanshu.ma <himanshu.ma@MMMSAHIMANSHUMA.local>
2022-06-22 23:48:57 +0530

    added login and logout functionality

--- base/urls.py
+++ base/urls.py
@@ -3,8 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
+    path("login/", views.loginPage, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
+    
     path("",views.home, name="home"),
     path('room/<str:pk>', views.room, name="room"),
+    
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<int:pk>', views.updateRoom, name="update-room"),
     path('delete-room/<int:pk>', views.deleteRoom, name="delete-room"),
--- base/views.py
+++ base/views.py
@@ -1,20 +1,34 @@
 from tkinter.tix import Form
 from django.shortcuts import redirect, render
-from .models import Room
+from django.db.models import Q
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from .models import Room, Topic
 from .forms import RoomForm
 
-rooms = [
-    {'id': 1, "name": "Lets learn python"},
-    {'id': 2, "name": "Design with me"},
-    {'id': 3, "name": "Frontend developers"},
-]
+
+# rooms = [
+#     {'id': 1, "name": "Lets learn python"},
+#     {'id': 2, "name": "Design with me"},
+#     {'id': 3, "name": "Frontend developers"},
+# ]
 # context = {'rooms': rooms}
 
 
 def home(request):
-    rooms = Room.objects.all()
+
+    q = request.GET.get('q') if request.GET.get("q") != None else ""
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+    # rooms = Room.objects.all()
+    topics = Topic.objects.all()
+    room_count = rooms.count()
     print(rooms)
-    context = {'rooms': rooms}
+    context = {'rooms': rooms, 'topics': topics, "room_count": room_count }
     return render(request, 'base/home.html', context)
 
 
@@ -61,3 +75,26 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username or password does not exists.')
+        user = authenticate(request, username=username,password=password)
+
+        if user is not None:
+            login(request, user) # add session in db and in browser
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exists.')
+   
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect("home")    
