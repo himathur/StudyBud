9ba5f3545c78f0d1123734aef2220b66a78873a1
himanshu.ma <himanshu.ma@MMMSAHIMANSHUMA.local>
2022-06-22 18:16:31 +0530

    added crud functionalities for room

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("",views.home, name="home"),
-    path('room/', views.room, name="room"),
+    path('room/<str:pk>', views.room, name="room"),
+    path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<int:pk>', views.updateRoom, name="update-room"),
+    path('delete-room/<int:pk>', views.deleteRoom, name="delete-room"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,17 +1,63 @@
-from django.shortcuts import render
+from tkinter.tix import Form
+from django.shortcuts import redirect, render
+from .models import Room
+from .forms import RoomForm
+
+rooms = [
+    {'id': 1, "name": "Lets learn python"},
+    {'id': 2, "name": "Design with me"},
+    {'id': 3, "name": "Frontend developers"},
+]
+# context = {'rooms': rooms}
 
 
 def home(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    print(rooms)
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    # room = None
+    # for i in rooms:
+    #     if i['id'] == int(pk):
+    #         room = i
 
-# TODO: clear later
-# with httpReponse
-# from django.http import HttpResponse
-# def home(request):
-#     return HttpResponse('Home Page')
-# def room(request):
-#     return HttpResponse("Room")
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == "POST":
+        # print(request.POST)
+        # print(request.POST.get('name'))
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
