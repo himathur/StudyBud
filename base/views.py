d8b5f9ee3b27b2bae3814f7d16e7069395c6c471
himanshu.ma <himanshu.ma@MMMSAHIMANSHUMA.local>
2022-07-02 15:32:07 +0530

    edit user functionality added

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Messages
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 
 def home(request):
@@ -168,3 +168,21 @@ def userProfile(request, pk):
     context = {"user": user, "rooms": rooms,
                "room_messages": room_messages, "topics": topics}
     return render(request, 'base/profile.html', context)
+
+@login_required(login_url="login")
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    return render(request,'base/update-user.html', {"form": form})
+
+
+def topicsPage(request):
+    topics = Topic.objects.filter()
+    return render(request, 'base/topics.html', {'topics': topics})
